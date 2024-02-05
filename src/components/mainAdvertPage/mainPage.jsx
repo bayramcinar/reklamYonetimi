c379b8e4c0a72a20881fb33668f416a31ff73151
Bayram Çınar <bayram200212@gmail.com>
2024-02-05 11:17:09 +0300

    colorUpdate

--- src/components/mainAdvertPage/mainPage.jsx
+++ src/components/mainAdvertPage/mainPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CreatedAdvertsTable from "./createdAdvertsTable";
 import AdvertTitleArea from "./advertTitleArea";
+import WhyUseAdvert from "./whyUseAdvert";
 
 function MainPage() {
   return (
     <div className="bg-gray-200">
       <AdvertTitleArea />
+      <WhyUseAdvert />
       <CreatedAdvertsTable />
     </div>
   );
---
+++ src/components/mainAdvertPage/whyUseAdvert.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function WhyUseAdvert() {
+  return (
+    <div>
+      <div className="titleArea">
+        <h1 className=" font-extrabold text-2xl text-center text-gray-700">
+          Neden Reklam Vermeliyim ?
+        </h1>
+        <h1 className="text-lg text-center mt-2 font-semibold">
+          Reklam vererek kullanıcı etkileşimizi arttırabilir bu sayede
+          satışlarınızda artış sağlayabilirsiniz
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default WhyUseAdvert;
